f66878edd76cbef3b02651f140b86ed3632e275c
ermacuor <ermacuor@gmail.com>
2025-07-10 22:08:35 -0400

    task

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = `© ${currentYear} - Erik Mark Cuiza Orellana - Bolivia`;
+
+
+const lastModified = document.lastModified;
+document.getElementById("lastupdated").textContent = `Last modification: ${lastModified}`;
