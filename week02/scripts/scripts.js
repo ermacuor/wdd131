15c24e6071d7092129a8f0904fc94509c36ab131
ermacuor <ermacuor@gmail.com>
2025-07-21 14:16:15 -0400

    buenas

---
+++ week02/scripts/scripts.js
@@ -0,0 +1,17 @@
+const buttona = document.querySelector('button');
+const input = document.querySelector('#favchap');   
+const list = document.querySelector('#list');
+buttona.addEventListener('click', function () {
+    if (input.value.trim() !== '') { 
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.focus();
+            deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+    });}
+});
