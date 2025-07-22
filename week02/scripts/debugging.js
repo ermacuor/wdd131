94a15c6a3303a6668db9ee0e1d6e0c81d21fa2b4
ermacuor <ermacuor@gmail.com>
2025-07-22 14:02:29 -0400

    IMAGES

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,16 +1,16 @@
 
-let radiusOutput = document.getElementById('radius');
-let areaOutput = document.querySelector('area');
+const radiusOutput = document.getElementById('radius');
+const areaOutput = document.querySelector('#area');
 
 let area = 0;
 const PI = 3.14159;
 
 let radius = 10;
-area.textContent = PI * radius * radius;
-radiusOutput = radius;
-areaOutput = area;
+area= PI * radius * radius;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
 radius = 20;
-area.textContent = PI * radius * radius;
+area= PI * radius * radius;
 radiusOutput.textContent = radius;
 areaOutput.textContent = area;
