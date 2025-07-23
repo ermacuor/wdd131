122f5649f9382644be65837196738a18f15e137a
ermacuor <ermacuor@gmail.com>
2025-07-23 12:58:14 -0400

    task

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,11 @@
 const hamButton = document.querySelector('#menu-button');
-const navigation = document.querySelector('#main-nav');
+const navigation = document.querySelector('.main-nav');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+const currentYear = document.querySelector('#currentyear');
+currentYear.textContent = "© " + new Date().getFullYear() + " ☬ " + " Erik Mark Cuiza Orellana" + " ☬ " + " Bolivia";
+
+document.getElementById("lastupdated").textContent = `Last modification: ${document.lastModified}`;
